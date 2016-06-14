9024ac37d5470ea452990bcd5db80c3c1146f5a0
Rhett Rogers <serenade.xs@icloud.com>
2016-06-14 10:03:17 -0700

    Adds example, and updates to swift 2.3

---
+++ Example/RichTextVC-iOS-Example/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RichTextVC-iOS-Example
+//
+//  Created by Rhett Rogers on 4/22/16.
+//  Copyright © 2016 LyokoTech. All rights reserved.
+//
+
+import UIKit
+import RichTextVC_iOS
+
+class ViewController: RichTextViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
